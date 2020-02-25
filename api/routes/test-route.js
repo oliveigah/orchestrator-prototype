5065fd06e75d4bf3a4b4ccb4b313fe0bfb68c85f
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2020-02-25 01:16:12 -0300

    error handling

--- api/routes/test-route.js
+++ api/routes/test-route.js
@@ -5,11 +5,17 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const result = {
+            success: true,
             hello: 'World',
         };
         res.status(200).json(result);
     } catch (error) {
-        res.status(401).json(error);
+        res.status(500).json({
+            success: false,
+            message: `${error.name}: ${error.message}`,
+        });
+        // eslint-disable-next-line no-console
+        console.log(error.stack);
     }
 });
 
