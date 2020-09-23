a93bbc48c6dbdced30143927ee05a164e2e26a0b
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2020-09-23 08:44:44 -0300

    eslint fixes

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ const runExpressServer = require('./app');
 
 console.log('info', `ENVIRONMENT: ${config.ENVIRONMENT}`);
 if (config.ENVIRONMENT === 'DEVELOPMENT') {
+  runExpressServer.orchestratorApp();
+  runExpressServer.service1App();
+  runExpressServer.service2App();
+  runExpressServer.service3App();
+} else {
+  if (cluster.isMaster) {
+    const cpuCount = os.cpus().length;
+    for (let j = 0; j < cpuCount; j++) {
+      cluster.fork();
+    }
+  } else {
     runExpressServer.orchestratorApp();
     runExpressServer.service1App();
     runExpressServer.service2App();
     runExpressServer.service3App();
-} else {
-    if (cluster.isMaster) {
-        const cpuCount = os.cpus().length;
-        for (let j = 0; j < cpuCount; j++) {
-            cluster.fork();
-        }
-    } else {
-        runExpressServer.orchestratorApp();
-        runExpressServer.service1App();
-        runExpressServer.service2App();
-        runExpressServer.service3App();
-    }
-    cluster.on('exit', (worker) => {
-        console.log(`Worker ${worker.id} died'`);
-        console.log('Staring a new one...');
-        cluster.fork();
-    });
+  }
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.id} died'`);
+    console.log('Staring a new one...');
+    cluster.fork();
+  });
 }
